a25c503ab54fe396d26b56f06095e9c205efd7b8
Farzana Tabassum <82896277+farzanatabassum@users.noreply.github.com>
2022-12-16 12:30:44 +0600

    install dependencies

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const next = require("next");
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const server = express();
+server.use(express.json());
+server.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app
+  .prepare()
+  .then(() => {
+    server.get("*", (req, res) => {
+      return handle(req, res);
+    });
+
+    server.listen(3000, (err) => {
+      if (err) throw err;
+      console.log("Starting http://localhost:3000");
+    });
+  })
+  .catch((ex) => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
